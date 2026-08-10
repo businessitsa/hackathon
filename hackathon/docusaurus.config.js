9558b3ed4fc467fab1af2b382767aefa5b7e499d
Business IT <businessitsa@outlook.com>
2026-08-10 12:33:41 +0200

    Update Base Url.

--- hackathon/docusaurus.config.js
+++ hackathon/docusaurus.config.js
@@ -21,10 +21,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://businessitsa.github.io',
+  url: 'https://hackathon.devmeetup.capetown',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/hackathon/',
+  // The custom domain serves the site from the root
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
