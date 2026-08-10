c7307bde04a52ad945be4091f13185eeb8621e84
Business IT <businessitsa@outlook.com>
2026-08-10 11:26:51 +0200

    Update theme.

--- hackathon/src/pages/index.js
+++ hackathon/src/pages/index.js
@@ -10,7 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -18,12 +18,12 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--cta button--lg"
             to="/docs/register">
             Register Your Team
           </Link>
           <Link
-            className="button button--outline button--secondary button--lg"
+            className="button button--outline button--ghost-green button--lg"
             to="/docs/about">
             Learn More
           </Link>
